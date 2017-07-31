cf0da0826ab94d0f59a6c2537efdc67121b87b0b
beacomni <beacomni@DSI-Yellows-MacBook-Pro.local>
2017-07-31 10:47:48 -0500

    first

---
+++ SwiftGetText.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftGetText.swift
+//  ObjectiveCAndSwift
+//
+//  Created by beacomni on 7/31/17.
+//  Copyright © 2017 beacomni. All rights reserved.
+//
+
+import UIKit
+
+class SwiftGetText: NSObject {
+
+}
